3d9f2a5cd769f110df9ba031b968b7843807aa76
gianlucapinna99 <gianlucapinna99@gmail.com>
2023-02-27 12:19:09 +0100

    Fixed

---
+++ JSnack1/main.js
@@ -0,0 +1,9 @@
+let number = 0;
+
+for (let i = 0; i < 5; i++) {
+    const input = Number(prompt("inserisci un numero"))
+    //sum = sum + input
+    sum += input;
+  }
+
+  console.log(sum)
--- JSnack3/main.js
+++ JSnack3/main.js
@@ -4,13 +4,14 @@ let nomiInvitati = ["paolo", "Marco", "Gaetano", "Laura", "Francesca"];
 
 let cognomiInvitati = ["Rossi", "Oldani", "Pennella", "Bisio", "Frascolla"];
 
+let invitati = [];
 
+console.log("LISTA INVITATI:");
 
 for (let i=0; i < 3; i++){
-    let nome = nomiInvitati[Math.floor(math.Random() * nomiInvitati.lenght)];
-    let cognome = cognomiInvitati[Math.floor(math.Random() * cognomiInvitati)];
-    invitati = (nome + "" + cognome);
+    let nome = nomiInvitati[Math.floor(Math.random() * nomiInvitati.length)];
+    let cognome = cognomiInvitati[Math.floor(Math.random() * cognomiInvitati.length)];
+    invitati = (nome + " " + cognome);
+    console.log(invitati);
 }
 
-console.log("lista invitati:")
-console.log(invitati)
